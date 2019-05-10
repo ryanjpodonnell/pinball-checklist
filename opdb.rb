df409417bdc8c91f2fe7764b2f24216fe81ed204
Ryan O'Donnell <ryan.jp.odonnell@gmail.com>
2019-05-09 17:43:46 -0700

    Move pin into it's own file

--- opdb.rb
+++ opdb.rb
@@ -1,5 +1,6 @@
 require 'json'
 require 'httparty'
+require_relative 'pin'
 
 API_TOKEN = ''
 
@@ -18,57 +19,6 @@ class OPDB
   def self.import_response(filename)
     file = File.read(filename)
     JSON.parse(file)
-  end
-end
-
-class Pin
-  TYPE_MAPPING = {
-    'em' => 'Electro-mechanical',
-    'me' => 'Mechanical',
-    'ss' => 'Solid State Electronic',
-  }
-
-  def initialize(pin_response)
-    @pin_response = pin_response
-  end
-
-  def name
-    @pin_response['name']
-  end
-
-  def manufacture_year
-    Date.parse(@pin_response['manufacture_date']).year
-  end
-
-  def manufacturer_name
-    @pin_response['manufacturer']['name']
-  end
-
-  def type
-    type_key = @pin_response['type']
-    TYPE_MAPPING[type_key]
-  end
-
-  def md_formatted_title
-    "- [ ] #{name} (#{manufacture_year})"
-  end
-
-  def md_formatted_type
-    "## #{type}"
-  end
-
-  def valid?
-    is_machine && physical_machine
-  end
-
-  private
-
-  def is_machine
-    @pin_response['is_machine']
-  end
-
-  def physical_machine
-    @pin_response['physical_machine']
   end
 end
 
---
+++ pin.rb
@@ -0,0 +1,50 @@
+class Pin
+  TYPE_MAPPING = {
+    'em' => 'Electro-mechanical',
+    'me' => 'Mechanical',
+    'ss' => 'Solid State Electronic',
+  }
+
+  def initialize(pin_response)
+    @pin_response = pin_response
+  end
+
+  def name
+    @pin_response['name']
+  end
+
+  def manufacture_year
+    Date.parse(@pin_response['manufacture_date']).year
+  end
+
+  def manufacturer_name
+    @pin_response['manufacturer']['name']
+  end
+
+  def type
+    type_key = @pin_response['type']
+    TYPE_MAPPING[type_key]
+  end
+
+  def md_formatted_title
+    "- [ ] #{name} (#{manufacture_year})"
+  end
+
+  def md_formatted_type
+    "## #{type}"
+  end
+
+  def valid?
+    is_machine && physical_machine
+  end
+
+  private
+
+  def is_machine
+    @pin_response['is_machine']
+  end
+
+  def physical_machine
+    @pin_response['physical_machine']
+  end
+end
